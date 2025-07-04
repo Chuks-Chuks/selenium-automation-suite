9cb57f595e03011d1e30ea7646d09b8df0be4a5f
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-07-04 23:55:59 +0100

    Added browser parametirisation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,47 @@
 import os
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as chromeservice
+from selenium.webdriver.firefox.service import Service as firefoxservice
+from selenium.webdriver.edge.service import Service as edgeservice
+from selenium.webdriver.edge.options import Options as EdgeOptions
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="Browser to run tests: chrome or firefox or edge or headless"
+    )
 
 
 @pytest.fixture
-def driver():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option('detach', True)
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+def driver(request):
+    browser = request.config.getoption('--browser')
+
+    if browser == 'chrome':
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option('detach', True)
+        driver = webdriver.Chrome(service=chromeservice(ChromeDriverManager().install()), options=chrome_options)
+        
+    elif browser == 'edge':
+        edge_options = EdgeOptions()
+        edge_options.add_experimental_option('detach', True)
+        driver = webdriver.Edge(service=edgeservice(EdgeChromiumDriverManager().install()), options=edge_options)
+        
+    elif browser == 'firefox':
+        driver = webdriver.Firefox(service=firefoxservice(GeckoDriverManager().install()))
+
+    elif browser == 'headless':
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(service=chromeservice(ChromeDriverManager().install()), options=options)
+
+    else:
+        raise ValueError(f'Browser type not supported {browser}')
+    
     driver.maximize_window()
     yield driver
     driver.quit()
--- tests/test_forms.py
+++ tests/test_forms.py
@@ -4,15 +4,16 @@ from pages.forms import Forms
 from .base_test import BaseTest
 
 
-class TestForms(BaseTest):
+class TestForms:
 
-    def test_forms(self):
-        forms = Forms(driver=self.driver)
+    def test_forms(self, driver):
+        forms = Forms(driver=driver)
         forms.fill_form_texts()
         forms.fill_form_gender_clicks()
         forms.click_dob()
         assert isinstance(forms.click_dob(), str)
         forms.fill_form_hobbies_click()
         forms.click_state_city()
+        driver.save_screenshot('../screenshots/passed/login_attempt.png')
         forms.click_submit()
 
