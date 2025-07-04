d9456f5e317d71be90f35741efc86e52e8a8ad87
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-07-04 14:25:15 +0100

    Added logging to the script

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -11,4 +12,16 @@ def driver():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.maximize_window()
     yield driver
-    # driver.quit()
+    driver.quit()
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    result = outcome.get_result()
+
+    if result.when == 'call' and result.failed:
+        driver = item.funcargs.get('driver')
+        if driver:
+            os.makedirs('../screenshots/failed/', exist_ok=True)
+            filename = f'screenshots/failed/{item.name}.png'
+            driver.save_screenshot(filename)
--- tests/test_login.py
+++ tests/test_login.py
@@ -2,14 +2,21 @@
 import sys
 sys.path.append('C:/Users/phili/selenium-exercise')
 from pages.login import Login
-    
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 
 def test_login(driver):
+    logging.info("Starting the test")
     login = Login(driver=driver)
     login.login_user()
+    logging.info("logging attempted")
 
     # Adding a screenshot after successfully logging in
     driver.save_screenshot('../screenshots/passed/login_attempt.png')
+
+    logging.info("Screenshot saved.")
 
     assert 'Products' in driver.page_source
 
